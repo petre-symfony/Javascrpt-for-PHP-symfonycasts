9562e65136fd7e425e3045e2aab7d9c678227faf
petrero <petrero@gmail.com>
2019-12-12 12:57:16 +0200

    12.4.Immediately Invoked Function Expression! {Creating a Private Object}
    The Self-Executing Function
    
    The way you do that is with - dun dun dun - an immediately invoked function expression. Also known by its friends as a self-executing function. Basically, that means we'll wrap all of our code inside a function... that calls itself. It's weird, but check it out: (function() {, then indent everything. At the bottom, add the }) and then ()
    
    What?
    
    There are two things to check out. First, all we're doing is creating a function: it starts on top, and ends at the bottom with the }. But by adding the (), we are immediately executing that function. We're creating a function and then calling it!
    
    Why on earth would we do this? Because! Variable scope in JavaScript is function based. When you create a variable with var, it's only accessible from inside of the function where you created it. If you have functions inside of that function, they have access to it too, but ultimately, that function is its home.
    
    Before, when we weren't inside of any function, our two variables effectively became global: we could access them from anywhere. But now that we're inside of a function, the RepLogApp and Helper variables are only accessible from inside of this self-executing function.
    
    This means that when we refresh, we get Helper is not defined. We just made the Helper variable private!
    
    Unfortunately... we also made our RepLogApp variable private, which means the code in our template will not work. We still need to somehow make RepLogApp available publicly, but not Helper. How? By taking advantage of the magical window object.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -1,67 +1,69 @@
-var RepLogApp = {
-	initialize: function($wrapper) {
-		this.$wrapper = $wrapper;
-		Helper.initialize(this.$wrapper);
+(function(){
+	var RepLogApp = {
+		initialize: function($wrapper) {
+			this.$wrapper = $wrapper;
+			Helper.initialize(this.$wrapper);
 
-		this.$wrapper.find('.js-delete-rep-log').on(
-			'click',
-			this.handleRepLogDelete.bind(this)
-		);
+			this.$wrapper.find('.js-delete-rep-log').on(
+				'click',
+				this.handleRepLogDelete.bind(this)
+			);
 
-		this.$wrapper.find('tbody tr').on(
-			'click',
-			this.handleRowClick.bind(this)
-		);
-	},
+			this.$wrapper.find('tbody tr').on(
+				'click',
+				this.handleRowClick.bind(this)
+			);
+		},
 
-	handleRepLogDelete: function(e) {
-		e.preventDefault();
+		handleRepLogDelete: function(e) {
+			e.preventDefault();
 
-		//e.target.className = e.target.className + ' text-danger';
-		var $link = $(e.currentTarget);
-		$link.addClass('text-danger');
+			//e.target.className = e.target.className + ' text-danger';
+			var $link = $(e.currentTarget);
+			$link.addClass('text-danger');
 
-		$link.find('.fa')
-			.removeClass('fa-trash')
-			.addClass('fa-spinner')
-			.addClass('fa-spin');
+			$link.find('.fa')
+				.removeClass('fa-trash')
+				.addClass('fa-spinner')
+				.addClass('fa-spin');
 
-		var deleteUrl = $link.data('url');
-		var $row = $link.closest('tr');
-		var self = this;
-		$.ajax({
-			url: deleteUrl,
-			method: 'DELETE',
-			success: function() {
-				$row.fadeOut('normal', function() {
-					$(this).remove();
-					self.updateTotalWeightLifted();
-				});
-			}
-		});
-	},
+			var deleteUrl = $link.data('url');
+			var $row = $link.closest('tr');
+			var self = this;
+			$.ajax({
+				url: deleteUrl,
+				method: 'DELETE',
+				success: function() {
+					$row.fadeOut('normal', function() {
+						$(this).remove();
+						self.updateTotalWeightLifted();
+					});
+				}
+			});
+		},
 
-	handleRowClick: function() {
-		console.log('row clicked!');
-	},
+		handleRowClick: function() {
+			console.log('row clicked!');
+		},
 
-	updateTotalWeightLifted: function() {
-		this.$wrapper.find('.js-total-weight').html(
-			Helper.calculateTotalWeight()
-		);
-	}
-};
+		updateTotalWeightLifted: function() {
+			this.$wrapper.find('.js-total-weight').html(
+				Helper.calculateTotalWeight()
+			);
+		}
+	};
 
-var Helper = {
-	initialize: function ($wrapper) {
-		this.$wrapper = $wrapper;
-	},
+	var Helper = {
+		initialize: function ($wrapper) {
+			this.$wrapper = $wrapper;
+		},
 
-	calculateTotalWeight: function() {
-		var totalWeight = 0;
-		this.$wrapper.find('tbody tr').each(function () {
-			totalWeight += $(this).data('weight');
-		});
-		return totalWeight;
-	}
-};
+		calculateTotalWeight: function() {
+			var totalWeight = 0;
+			this.$wrapper.find('tbody tr').each(function () {
+				totalWeight += $(this).data('weight');
+			});
+			return totalWeight;
+		}
+	};
+})();
