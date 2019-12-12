1e3333d2af76b6b84ddcaccdfc29e34b0ce93a8a
petrero <petrero@gmail.com>
2019-12-12 14:29:01 +0200

    16.4.prototype Versus __proto__{Creating Multiple Instances}
    Creating Multiple Instances
    
    The whole point of this new constructor and prototype setup is so that we could have multiple instances of our Helper object. The prototype is just the key to take advantage of it.
    
    To prove it all works, add var helper2 = new Helper() and pass it a different , like the footer on our page.
    
    Since the footer doesn't have any rows that have weight on it, this should return zero. Log that: this.helper.calculateTotalWeight() and helper2.calculateTotalWeight()
    
    Try that! Cool! 157.5 and of course, zero.

--- web/assets/js/RepLogApp.js
+++ web/assets/js/RepLogApp.js
@@ -5,13 +5,12 @@
 		initialize: function($wrapper) {
 			this.$wrapper = $wrapper;
 			this.helper = new Helper(this.$wrapper);
-
+			var helper2 = new Helper($('footer'));
 			console.log(
-				'foo'.__proto__,
-				[].__proto__,
-				(new Date()).__proto__
+				this.helper.calculateTotalWeight(),
+				helper2.calculateTotalWeight()
 			);
-			
+
 			this.$wrapper.find('.js-delete-rep-log').on(
 				'click',
 				this.handleRepLogDelete.bind(this)
