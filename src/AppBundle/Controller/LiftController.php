150f559e49aa68102ff815d149464aa52a50fa5a
petrero <petrero@gmail.com>
2019-12-12 19:32:44 +0200

    21.1.Proper JSON API Endpoint Setup
    It's time to graduate from this old-school AJAX approach where the server sends us HTML, to one where the server sends us ice cream! I mean, JSON!
    
    First, in LiftController::indexAction(), let's remove the two AJAX if statements from before: we won't use them anymore.
    
    In fact, we're not going to use this endpoint at all. So, close this file.
    
    Next, head to your browser, refresh, and view the source. Find the <form> element and copy the entire thing. Then back in your editor, find _form.html.twig and completely replace this file with that

--- src/AppBundle/Controller/LiftController.php
+++ src/AppBundle/Controller/LiftController.php
@@ -26,13 +26,6 @@ class LiftController extends BaseController {
       $em->persist($repLog);
       $em->flush();
 
-	    // return a blank form after success
-	    if ($request->isXmlHttpRequest()) {
-		    return $this->render('lift/_repRow.html.twig', [
-			    'repLog' => $repLog
-		    ]);
-	    }
-
       $this->addFlash('notice', 'Reps crunched!');
 
       return $this->redirectToRoute('lift');
@@ -45,15 +38,6 @@ class LiftController extends BaseController {
     foreach ($repLogs as $repLog) {
       $totalWeight += $repLog->getTotalWeightLifted();
     }
-
-	  // render just the form for AJAX, there is a validation error
-	  if ($request->isXmlHttpRequest()) {
-		  $html = $this->renderView('lift/_form.html.twig', [
-			  'form' => $form->createView()
-		  ]);
-
-		  return new Response($html, 400);
-	  }
 
     return $this->render('lift/index.html.twig', array(
       'form' => $form->createView(),
