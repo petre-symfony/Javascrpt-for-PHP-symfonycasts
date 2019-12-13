561a0ea7fe959f07d0945631fd0ebf0329671d3f
petrero <petrero@gmail.com>
2019-12-13 09:03:23 +0200

    26.3.Full-JavaScript Rendering & FOSJsRoutingBundle{Using FOSJsRoutingBundle}
    While Jordi is wrapping our package with a bow, let's finish the install instructions. Copy the new bundle line, and add that to app/AppKernel.php. We also need to import some routes: paste this into app/config/routing.yml. Finally, we need to add two script tags to our page. Open base.html.twig and paste them at the bottom
    
    This bundle exposes a global variable called Routing. And you can use that Routing variable to generate links in the same way that we use the path function in Twig templates: just pass it the route name and parameters.
    
    Check the install process. Ding!
    
     Tip
    
    If you have a JavaScript error where Routing is not defined, you may need to run:
    
    php bin/console assets:install

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel{
       new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
 
       new AppBundle\AppBundle(),
+	    new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
     ];
 
     if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
